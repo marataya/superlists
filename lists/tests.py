59fa66d24f44d346731df5e1c23a259ee9a70784
Marat Ayaibergenov <marataya@gmail.com>
2023-08-31 02:17:11 +0600

    modified unit test to use django test client

--- lists/tests.py
+++ lists/tests.py
@@ -12,8 +12,8 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertTrue(html.endswith('</html>'))
-        self.assertIn('<title>To-Do</title>', html)
+        response = self.client.get('/')
+        self.assertContains(response, '<title>To-Do</title>')
+        self.assertContains(response, '<html>')
+        self.assertContains(response, '</html>')
+        self.assertTemplateUsed(response, "home.html")
