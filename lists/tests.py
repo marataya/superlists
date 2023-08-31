7be16f89385a14cf71648599b037fd4165ca9d7a
Marat Ayaibergenov <marataya@gmail.com>
2023-09-01 01:07:09 +0600

    modify functional_test to handle POST request and associated changes in template tesunit tests and view

--- lists/tests.py
+++ lists/tests.py
@@ -17,3 +17,8 @@ class HomePageTest(TestCase):
         self.assertContains(response, '<html>')
         self.assertContains(response, '</html>')
         self.assertTemplateUsed(response, "home.html")
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={"item_text": "A new list item"})
+        self.assertContains(response, "A new list item")
+        self.assertTemplateUsed(response, 'home.html')
